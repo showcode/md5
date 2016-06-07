3af49c90ddeca50418e807ceea15d98410524b64
showcode <unspam@ya.ru>
2016-06-07 22:30:28 +0300

    Replace "magic numbers"

--- main.c
+++ main.c
@@ -8,19 +8,21 @@
 #include "md5.h"
 
 #define PATH_DELIMITER '/'
+#define DIGEST_SIZE (16)
+#define BUFFER_SIZE (4 * 1024)
 
 void calc_md5(const char * filename, char * digest_str)
 {
-    FILE * file = fopen(filename, "r");
-    if (file != NULL)
-    {
-        const int buff_size = 4 * 1024;
-        unsigned char buff[buff_size], digest[16];
-        MD5_CTX ctx;
-        int num;
+    FILE * file;
+    char buff[BUFFER_SIZE];
+    unsigned char digest[DIGEST_SIZE];
+    MD5_CTX ctx;
+    int num;
 
+    if ( (file = fopen(filename, "r")) != NULL )
+    {
         MD5Init(&ctx);
-        while ( (num = fread(buff, 1, buff_size, file)) > 0 )
+        while ( (num = fread(buff, 1, BUFFER_SIZE, file)) > 0 )
         {
             MD5Update(&ctx, buff, num);
         }
@@ -28,7 +30,7 @@ void calc_md5(const char * filename, char * digest_str)
 
         fclose(file);
 
-        for (num = 0; num < 16; num++)
+        for (num = 0; num < DIGEST_SIZE; num++)
         {
             sprintf(&digest_str[num * 2], "%.2x", digest[num]);
         }
@@ -39,15 +41,7 @@ int match(const char * filename, const char * filemask)
 {
     while (*filemask)
     {
-        if (*filemask == '?')
-        {
-            if (!*filename)
-                return 0;
-
-            filename++;
-            filemask++;
-        }
-        else if (*filemask == '*')
+        if (*filemask == '*')
         {
             if (match(filename, filemask + 1))
                 return 1;
@@ -56,6 +50,14 @@ int match(const char * filename, const char * filemask)
                 return 1;
 
             return 0;
+        }
+        else if (*filemask == '?')
+        {
+            if (!*filename)
+                return 0;
+
+            filename++;
+            filemask++;
         }
         else
         {
@@ -72,16 +74,16 @@ int main(int argc, char * argv[])
     if (argc < 2)
     {
         printf("usage:\rchksum <path/filemask>\r");
-        return 0;
+        return 1;
     }
 
     char * fileparam = argv[1];
     char * delim, * wildcard;
     char filename[PATH_MAX];
-    int pathlen;
+    int pathlen = 0;
 
-    delim = strrchr(fileparam, PATH_DELIMITER);
-    pathlen = (delim != NULL) ? delim - fileparam + 1 : 0;
+    if ( (delim = strrchr(fileparam, PATH_DELIMITER)) != NULL )
+        pathlen = delim - fileparam + 1;
     strncpy(filename, fileparam, pathlen);
     wildcard = &fileparam[pathlen];
 
@@ -91,7 +93,7 @@ int main(int argc, char * argv[])
     {
         if ( (de->d_type != DT_DIR) && match(de->d_name, wildcard) )
         {
-            char digest_str[16 * 2 + 1];
+            char digest_str[DIGEST_SIZE * 2 + 1];
 
             strcpy(&filename[pathlen], de->d_name);
             calc_md5(filename, digest_str);
